60d3b569b283fc7929624d4708b3ac0e499bd367
soyenle <34003821+soyenle@users.noreply.github.com>
2021-12-01 14:37:26 +0100

    Update events.js

--- events.js
+++ events.js
@@ -1,3 +1,46 @@
+// Component to make a popup window for the quiz.
+AFRAME.registerComponent('listener1', {
+ init: function () {
+    this.el.addEventListener('click', function (evt) {
+            document.getElementById("btn").textContent = "Öffne Quiz1";
+            document.getElementById("btn").onclick = Quiz1;    
+    }); } });
+
+     AFRAME.registerComponent('listener2', {
+  init: function () {
+    this.el.addEventListener('click', function (evt) {
+            document.getElementById("btn").textContent = "Öffne Quiz2";
+            document.getElementById("btn").onclick = Quiz2;      
+    }); }});
+
+      AFRAME.registerComponent('listener3', {
+  init: function () {
+    this.el.addEventListener('click', function (evt) {
+            document.getElementById("btn").textContent = "Öffne Quiz3";
+            document.getElementById("btn").onclick = Quiz3;      
+    }); }});
+
+      AFRAME.registerComponent('listener4', {
+  init: function () {
+    this.el.addEventListener('click', function (evt) {
+            document.getElementById("btn").textContent = "Öffne Quiz4";
+            document.getElementById("btn").onclick = Quiz4;      
+    }); }});
+
+  AFRAME.registerComponent('listener5', {
+  init: function () {
+    this.el.addEventListener('click', function (evt) {
+            document.getElementById("btn").textContent = "Öffne Quiz5";
+            document.getElementById("btn").onclick = Quiz5;      
+    }); }});
+
+      AFRAME.registerComponent('listener6', {
+  init: function () {
+    this.el.addEventListener('click', function (evt) {
+            document.getElementById("btn").textContent = "Öffne Quiz6";
+            document.getElementById("btn").onclick = Quiz6;      
+    }); }});
+
 
         function Quiz0() {           
             window.alert("Richte die Kamera auf einen Marker bis ein Modell erscheint \nBerühre mit dem mittleren Kreis ein Modell \nDanach lässt sich ein Quizfenster öffnen \nRotation und Skalierung der Modelle anhand Touch Bewegung möglich" );
@@ -20,49 +63,6 @@
          function Quiz6() {           
             window.open("https://yahoo.de/");
          }
-
-// Component to make a popup window for the quiz.
-AFRAME.registerComponent('listener1', {
- init: function () {
-    this.el.addEventListener('mouseenter', function (evt) {
-            document.getElementById("btn").textContent = "Öffne Quiz1";
-            document.getElementById("btn").onclick = Quiz1;    
-    }); } });
-
-     AFRAME.registerComponent('listener2', {
-  init: function () {
-    this.el.addEventListener('mouseenter', function (evt) {
-            document.getElementById("btn").textContent = "Öffne Quiz2";
-            document.getElementById("btn").onclick = Quiz2;      
-    }); }});
-
-      AFRAME.registerComponent('listener3', {
-  init: function () {
-    this.el.addEventListener('mouseenter', function (evt) {
-            document.getElementById("btn").textContent = "Öffne Quiz3";
-            document.getElementById("btn").onclick = Quiz3;      
-    }); }});
-
-      AFRAME.registerComponent('listener4', {
-  init: function () {
-    this.el.addEventListener('mouseenter', function (evt) {
-            document.getElementById("btn").textContent = "Öffne Quiz4";
-            document.getElementById("btn").onclick = Quiz4;      
-    }); }});
-
-  AFRAME.registerComponent('listener5', {
-  init: function () {
-    this.el.addEventListener('mouseenter', function (evt) {
-            document.getElementById("btn").textContent = "Öffne Quiz5";
-            document.getElementById("btn").onclick = Quiz5;      
-    }); }});
-
-      AFRAME.registerComponent('listener6', {
-  init: function () {
-    this.el.addEventListener('mouseenter', function (evt) {
-            document.getElementById("btn").textContent = "Öffne Quiz6";
-            document.getElementById("btn").onclick = Quiz6;      
-    }); }});
 
  
 
